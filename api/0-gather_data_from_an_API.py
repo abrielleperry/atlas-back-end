adc7204dec4308f26e4594ae5e9cda943e9c450e
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-17 17:40:32 -0500

    adding count done tasks func

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,22 +3,31 @@ from flask import request
 import sys
 
 def employee_data():
-    employee_user = request.get("https://jsonplaceholder.typicode.com/users")
-    employee_todo = request.get("https://jsonplaceholder.typicode.com/todos")
-    return employee_user.json(), employee_todo.json()
+    users_data = request.get("https://jsonplaceholder.typicode.com/users")
+    todos_data = request.get("https://jsonplaceholder.typicode.com/todos")
+    return users_data.json(), todos_data.json()
 
     
-def employee_name(employee_user, input_id):
-    for user in employee_user:
-        if user['id'] == input_id:
+def employee_name(users_data, employee_id):
+    for user in users_data:
+        if user['id'] == employee_id:
             return user['name']
     return None
 
-def todo_progress(employee_todo, input_id):
-    
+def count_done_tasks(todos_data, employee_id):
+    number_of_done_tasks = sum(1 for todo in todos_data 
+                     if todo['userId'] == employee_id)
+    return number_of_done_tasks
 
+def count
 
 if __name__ == "__main__":
-    employee_name, employee_todo = employee_data()
+    users_data, todos_data = employee_data()
 input_id = int(sys.argv[1])
 name = 
+
+if name:
+    print(f"Employee {name} is done with tasks({/}).")
+else:
+    
+    Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):
