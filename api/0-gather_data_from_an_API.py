8078d053ec8c5bd11e482a611be07a9be699375a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-17 16:05:27 -0500

    get users and todos

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+from flask import Flask, request
+import sys
+
+def employee():
+    user = request.get("https://jsonplaceholder.typicode.com/users")
+    todo = request.get("https://jsonplaceholder.typicode.com/todos")
