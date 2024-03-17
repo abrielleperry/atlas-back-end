06cb96ca5ceeda6b211eba9c3bd2852919e42e76
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-17 16:43:33 -0500

    adding employee name

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-from flask import Flask, request
+from flask import request
 import sys
 
-def employee():
-    user = request.get("https://jsonplaceholder.typicode.com/users")
-    todo = request.get("https://jsonplaceholder.typicode.com/todos")
+def employee_data():
+    employee_user = request.get("https://jsonplaceholder.typicode.com/users")
+    employee_todo = request.get("https://jsonplaceholder.typicode.com/todos")
+    return employee_user.json(), employee_todo.json()
+
+    
+def employee_name(employee_user, input_id):
+    for user in employee_user:
+        if user['id'] == input_id:
+            return user['name']
+    return None
+    
+    
+ #   Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
