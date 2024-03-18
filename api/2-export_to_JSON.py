a56e6395bede5f5f5dfc675465a6001c6b098e98
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-18 15:01:33 -0500

    pep8

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -29,9 +29,8 @@ if __name__ == "__main__":
             if data['userId'] == input_id:
                 info[input_id].append({
                     "task": data['title'],
-                    "completed": data['completed'], 
+                    "completed": data['completed'],
                     "username": username
                 })
 
-                    
-                json.dump(['input_id', ":", 
+                json.dump(['input_id', ":",
