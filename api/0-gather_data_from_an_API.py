af3c36c167de3786f0bc097c20bbb24b6946f720
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-18 12:09:45 -0500

    rm .format

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -48,6 +48,6 @@ if __name__ == "__main__":
     comp_tasks = count_done_tasks(todos_data, input_id)
     total_tasks = count_total_tasks(todos_data, input_id)
 
-    print("Employee {} is done with tasks({}/{}).")
+    print(f"Employee {name} is done with tasks({comp_tasks}/{total_tasks}).".format(name, comp_tasks, total_tasks))
     second = completed_task_title(todos_data, input_id)
-    print("{}".format(second))
+    print("\t {}".format(second))
