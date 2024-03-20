b62d7c627ff0f6ac70163e18d322c93751be2e93
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-19 21:15:05 -0500

    pep8

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -14,20 +14,20 @@ def fetch_employee_data():
 if __name__ == "__main__":
     users_data, todos_data = fetch_employee_data()
 
-# maps user id to username and list of tasks
+# maps user id to username in usernames dictionary
     info = {}
     usernames = {user['id']: user['username'] for user in users_data}
     with open(f'todo_all_employees.json', 'w', newline="") as jsonfile:
         for user_id, username in usernames.items():
             info[user_id] = []
 
-# maps todo item to user id in info dictionary
+# maps user id to list of dictionaries in info dictionary
         for data in todos_data:
             if data['userId'] in info:
                 info[data['userId']].append({
                     'username': usernames[data['userId']],
-                    'task':data['title'],
-                    'completed':data['completed']
+                    'task': data['title'],
+                    'completed': data['completed']
                 })
 
         json.dump(info, jsonfile)
